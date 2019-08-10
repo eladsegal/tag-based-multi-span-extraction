f911cfc0452c38d9f82c125d2c536c922ae5e6bd
Avia <avia.efrat@gmail.com>
2019-08-10 12:59:59 +0300

    Add BIO label fields to span-typed Instances
    This implementation ignores questions with invalid answer type and span questions with invalid spans, as these tasks were transferred to a dedicated module.
    On the DROP training set, it returns 64282 question Instances.

--- src/bert_reader.py
+++ src/bert_reader.py
@@ -1,19 +1,16 @@
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from allennlp.data.token_indexers.token_indexer import TokenIndexer
-from allennlp.data.tokenizers import Token, Tokenizer, WordTokenizer
+from allennlp.data.tokenizers import Token, Tokenizer
 from allennlp.data.instance import Instance
-from allennlp.data.fields import Field, TextField, IndexField, LabelField, ListField, \
-                                 MetadataField, SequenceLabelField, SpanField, ArrayField
+from allennlp.data.fields import Field, TextField, MetadataField, SequenceLabelField, ArrayField
 
 from collections import defaultdict
 import json
-import logging
 from overrides import overrides
-from typing import Dict, List, Tuple, Any, Optional
+from typing import Dict, List, Tuple, Optional
 
 import numpy as np
 
-logger = logging.getLogger(__name__)
 
 @DatasetReader.register("multi_span_drop")
 class BertDropReader(DatasetReader):
@@ -39,7 +36,7 @@ class BertDropReader(DatasetReader):
             passage_text = passage_data['passage']
             passage_tokens = self.tokenizer.tokenize(passage_text)
 
-            #Note: NAQANET addded split by hyphen here
+            # NAQANET added split by hyphens here
 
             for qa_pair in passage_data["qa_pairs"]:
                 question_id = qa_pair["query_id"]
@@ -48,10 +45,6 @@ class BertDropReader(DatasetReader):
 
                 answer_type = get_answer_type(answer)
                 if answer_type not in self.answer_types:
-                    if answer_type is None:
-                        logger.warning(f'answer for question `{question_text}` has no valid answer type.\n'
-                                       f'valid answer types: {self.answer_types}\n'
-                                       f'query_id: {question_id}')
                     continue
 
                 answer_annotations: List[Dict] = list()
@@ -70,7 +63,7 @@ class BertDropReader(DatasetReader):
                 if instance is not None:
                     instances.append(instance)
 
-                if self.max_instances >= len(instances):
+                if 0 < self.max_instances <= len(instances):
                     break
 
         return instances
@@ -96,14 +89,29 @@ class BertDropReader(DatasetReader):
         qp_field = TextField(qp_tokens, self.token_indexers)
         fields['question_and_passage'] = qp_field
 
-        if answer_type == 'single_span' or 'multiple_span':
-            # We use first answer annotation, like in NABERT
-            bio_labels, bio_mask = self.generate_bio_labels(qp_tokens, answer_annotations[0]['spans'])
-            if bio_labels is None or bio_mask is None:
-                return None
+        if answer_type in ('single_span', 'multiple_span'):
+            qp_token_indices: Dict[Token, List[int]] = defaultdict(list)
+            for i, token in enumerate(qp_tokens):
+                qp_token_indices[token].append(i)
 
-        fields['multi_span_bio'] = ArrayField(array=bio_labels)
-        fields['multi_span_bio_mask'] = ArrayField(array=bio_mask)
+            # We use the first answer annotation, like in NABERT
+            answer_texts = answer_annotations[0]['spans']
+
+            spans = []
+            for answer_text in answer_texts:
+                answer_tokens = self.tokenizer.tokenize(answer_text)
+                answer_span = find_span(answer_tokens, qp_token_indices, len(qp_field))
+                if len(answer_span) != 1:
+                    return None
+                spans.extend(answer_span)
+
+            bio_labels = create_bio_labels(spans, len(qp_field))
+            fields['span_labels'] = SequenceLabelField(bio_labels, sequence_field=qp_field)
+
+            # in a word broken into pieces, subsequent pieces should be ignored when calculating the loss
+            wordpiece_mask = [not token.text.startswith('##') for token in qp_tokens]
+            wordpiece_mask = np.array(wordpiece_mask)
+            fields['span_wordpiece_mask'] = ArrayField(wordpiece_mask)
 
         metadata = {
             'original_passage': passage_text, 
@@ -119,30 +127,19 @@ class BertDropReader(DatasetReader):
         fields['metadata'] = MetadataField(metadata)
         return Instance(fields)
 
-    def generate_bio_labels(self, qp_tokens, answer_texts):
 
-        qp_token_indices: Dict[Token, List[int]] = defaultdict(list)
-        for i, token in enumerate(qp_tokens):
-            qp_token_indices[token].append(i)
-
-        spans = list()
-        for answer_text in answer_texts:
-            answer_tokens = self.tokenizer.tokenize(answer_text)
-            span = find_span(answer_tokens, qp_token_indices, len(qp_tokens))
-
-            if len(span) != 1:
-                if len(span) == 0:
-                    logger.warning(f'no match was found for answer `{answer_text}` in tokens: `{qp_tokens}`')
-                # if len(span) > 1:
-                #     logger.warning(f'multiple matches were found for answer `{answer_text}` in tokens: `{qp_tokens}`\n'
-                #                    f'span indices: `{span}`')
-                return None, None
-            spans += span
-
-        # create bio labels from the spans
-        # create mask from the spans
-
-        return None, None
+def get_answer_type(answer: Dict):
+    # TODO make sure that only a single answer type is assigned a value
+    if answer['number']:
+        return 'number'
+    elif answer['spans']:
+        if len(answer['spans']) == 1:
+            return 'single_span'
+        return 'multiple_span'
+    elif any(answer['date'].values()):
+        return 'date'
+    else:
+        return None
 
 
 def find_span(answer_tokens: List[Token], qp_token_indices: Dict[Token, List[int]],
@@ -162,15 +159,17 @@ def find_span(answer_tokens: List[Token], qp_token_indices: Dict[Token, List[int
     return span
 
 
+def create_bio_labels(spans: List[Tuple[int, int]], n_labels: int):
 
-def get_answer_type(answer):
-    if answer['number']:
-        return 'number'
-    elif answer['spans']:
-        if len(answer['spans']) == 1:
-            return 'single_span'
-        return 'multiple_span'
-    elif any(answer['date'].values()):
-        return 'date'
-    else:
-        return None
+    # initialize all labels to O
+    labels = [0] * n_labels
+
+    for span in spans:
+        start = span[0]
+        end = span[1]
+        # create B labels
+        labels[start] = 1
+        # create I labels
+        labels[start+1:end+1] = [2] * (end - start)
+
+    return labels
