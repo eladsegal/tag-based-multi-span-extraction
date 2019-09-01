8721d79d75c7f5e0e064ae8f6d0e1d42c31cd64b
Elad Segal <eladsegal@users.noreply.github.com>
2019-09-01 07:13:38 +0300

    mask also [SEP] and [CLS] for multispan

--- src/nabertplusplus/nabertplusplus.py
+++ src/nabertplusplus/nabertplusplus.py
@@ -170,16 +170,11 @@ class NumericallyAugmentedBERTPlusPlus(Model):
         passage_mask = seqlen_ids * pad_mask * cls_sep_mask
         # Shape: (batch_size, seqlen)
         question_mask = (1 - seqlen_ids) * pad_mask * cls_sep_mask
-        # question_and_passage_mask = question_mask | passage_mask
-        # if bio_wordpiece_mask is None:
-        #     multispan_mask = question_and_passage_mask
-        # else:
-        #     multispan_mask = question_and_passage_mask * bio_wordpiece_mask
-
+        question_and_passage_mask = question_mask | passage_mask
         if bio_wordpiece_mask is None:
-            multispan_mask = pad_mask
+            multispan_mask = question_and_passage_mask
         else:
-            multispan_mask = pad_mask & bio_wordpiece_mask
+            multispan_mask = question_and_passage_mask * bio_wordpiece_mask
 
         # Shape: (batch_size, seqlen, bert_dim)
         bert_out, _ = self.BERT(question_passage_tokens, seqlen_ids, pad_mask)
