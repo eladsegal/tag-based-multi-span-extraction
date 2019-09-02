22129bb3d273bbb7adb13fe4f554e40ad75c1642
morshoham <morshoh@gmail.com>
2019-09-02 12:41:18 +0300

    archive for non best weights

--- archive.py
+++ archive.py
@@ -1,4 +1,7 @@
 from allennlp.models.archival import archive_model
 import sys
 
-archive_model(sys.argv[1], archive_path=sys.argv[2])
+if len(sys.argv) < 4:
+    archive_model(sys.argv[1], archive_path=sys.argv[2])
+else:
+    archive_model(sys.argv[1], weights = sys.argv[3], archive_path=sys.argv[2])
